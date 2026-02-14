8964c84cf902c00c74176182d8dec555b6dc12ae
+ <+>
2026-02-14 13:12:53 +0300

    Fix: Bind Flask to 0.0.0.0: for Render compatibility

--- app.py
+++ app.py
@@ -59,4 +59,5 @@ def create_app(config=None):
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
